e7dd0b79a59ac6f334238a686e53a0ca352d481f
Administrator <Administrator@BIELNB1822>
2025-05-05 11:22:57 +0800

    Add project files.

---
+++ FrameworkWindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace FrameworkWindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.highlightLabel4 = new FrameworkWindowsFormsApp1.HighlightLabel();
+            this.highlightLabel3 = new FrameworkWindowsFormsApp1.HighlightLabel();
+            this.highlightLabel2 = new FrameworkWindowsFormsApp1.HighlightLabel();
+            this.highlightLabel1 = new FrameworkWindowsFormsApp1.HighlightLabel();
+            this.SuspendLayout();
+            // 
+            // highlightLabel4
+            // 
+            this.highlightLabel4.AutoSize = true;
+            this.highlightLabel4.Font = new System.Drawing.Font("SimSun", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.highlightLabel4.Location = new System.Drawing.Point(300, 16);
+            this.highlightLabel4.Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
+            this.highlightLabel4.Name = "highlightLabel4";
+            this.highlightLabel4.Size = new System.Drawing.Size(83, 34);
+            this.highlightLabel4.TabIndex = 5;
+            this.highlightLabel4.Text = "项目";
+            // 
+            // highlightLabel3
+            // 
+            this.highlightLabel3.AutoSize = true;
+            this.highlightLabel3.Font = new System.Drawing.Font("SimSun", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.highlightLabel3.Location = new System.Drawing.Point(205, 16);
+            this.highlightLabel3.Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
+            this.highlightLabel3.Name = "highlightLabel3";
+            this.highlightLabel3.Size = new System.Drawing.Size(83, 34);
+            this.highlightLabel3.TabIndex = 4;
+            this.highlightLabel3.Text = "视图";
+            // 
+            // highlightLabel2
+            // 
+            this.highlightLabel2.AutoSize = true;
+            this.highlightLabel2.Font = new System.Drawing.Font("SimSun", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.highlightLabel2.Location = new System.Drawing.Point(110, 16);
+            this.highlightLabel2.Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
+            this.highlightLabel2.Name = "highlightLabel2";
+            this.highlightLabel2.Size = new System.Drawing.Size(83, 34);
+            this.highlightLabel2.TabIndex = 3;
+            this.highlightLabel2.Text = "编辑";
+            // 
+            // highlightLabel1
+            // 
+            this.highlightLabel1.AutoSize = true;
+            this.highlightLabel1.Font = new System.Drawing.Font("SimSun", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.highlightLabel1.Location = new System.Drawing.Point(15, 16);
+            this.highlightLabel1.Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
+            this.highlightLabel1.Name = "highlightLabel1";
+            this.highlightLabel1.Size = new System.Drawing.Size(83, 34);
+            this.highlightLabel1.TabIndex = 2;
+            this.highlightLabel1.Text = "文件";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.highlightLabel4);
+            this.Controls.Add(this.highlightLabel3);
+            this.Controls.Add(this.highlightLabel2);
+            this.Controls.Add(this.highlightLabel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private HighlightLabel highlightLabel1;
+        private HighlightLabel highlightLabel2;
+        private HighlightLabel highlightLabel3;
+        private HighlightLabel highlightLabel4;
+    }
+}
+
---
+++ FrameworkWindowsFormsApp1/HighlightLabel.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FrameworkWindowsFormsApp1
+{
+    internal class HighlightLabel : Label
+    {
+        private static readonly int s_backColorAlphaDecreaseIntervalInMilliseconds = 10;
+
+        private static readonly int s_maxBackColorAlpha = 255;
+        private static readonly int s_backColorAlphaDecrement = 15;
+        private static readonly int s_minBackColorAlpha = 0;
+
+        private int _backColoralpha = s_maxBackColorAlpha;
+        private CancellationTokenSource _backColorFadeOutCancellationTokenSource;
+
+        public HighlightLabel()
+        {
+            MouseEnter += highlightLabel_MouseEnter;            
+            MouseLeave += highlightLabel_MouseLeave;
+        }
+
+        private void highlightLabel_MouseEnter(object sender, EventArgs e)
+        {
+            if (_backColorFadeOutCancellationTokenSource != null)
+            {
+                _backColorFadeOutCancellationTokenSource.Cancel();
+                _backColorFadeOutCancellationTokenSource.Dispose();
+            }
+
+            BackColor = Color.FromArgb(s_maxBackColorAlpha, Color.Red);
+        }
+
+        private void highlightLabel_MouseLeave(object sender, EventArgs e)
+        {
+            _backColorFadeOutCancellationTokenSource = new CancellationTokenSource();
+            _backColoralpha = s_maxBackColorAlpha;
+
+            BackColorFadeOut();
+        }
+
+        private void BackColorFadeOut()
+        {
+            Task task = Task
+                .Delay(s_backColorAlphaDecreaseIntervalInMilliseconds, _backColorFadeOutCancellationTokenSource.Token)
+                .ContinueWith(t =>
+                {
+                    if (_backColorFadeOutCancellationTokenSource.IsCancellationRequested)
+                    {
+                        return;
+                    }
+
+                    _backColoralpha = _backColoralpha - s_backColorAlphaDecrement;
+                    if (_backColoralpha < 0)
+                    {
+                        _backColoralpha = 0;
+                    }
+
+                    Invoke((Action)(() => BackColor = Color.FromArgb(_backColoralpha, Color.Red)));
+                }, _backColorFadeOutCancellationTokenSource.Token);
+
+            if (_backColoralpha > 0 && !_backColorFadeOutCancellationTokenSource.IsCancellationRequested)
+            {
+                task.ContinueWith(t => BackColorFadeOut(), _backColorFadeOutCancellationTokenSource.Token);
+            }
+        }
+    }
+}
